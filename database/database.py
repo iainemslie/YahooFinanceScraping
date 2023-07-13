28cb00050098abf1ff5cdee037e3783045a2eba3
Iain Emslie <iainemslie@gmail.com>
2023-07-12 21:02:20 -0400

    Added multi .csv parsing and insertion but need to fix bug

--- database/database.py
+++ database/database.py
@@ -2,6 +2,8 @@ import mysql.connector
 from mysql.connector import Error
 
 import csv
+import os
+import time
 
 def create_connection(host_name, user_name, user_password):
     connection = None
@@ -10,7 +12,9 @@ def create_connection(host_name, user_name, user_password):
             host = host_name,
             user = user_name,
             passwd = user_password,
-            autocommit=True
+            autocommit = True,
+            pool_reset_session=True,
+            pool_size = 32
         )
         print("Connection to MySQL Database successful")
     except Error as e:
@@ -21,9 +25,9 @@ def create_connection(host_name, user_name, user_password):
 def execute_query(connection, query):
     cursor = connection.cursor()
     try:
-        cursor.execute(query)
+        cursor.execute(query, multi=True)
         # connection.commit()
-        print("Query executed successfully")
+        #print("Query executed successfully")
     except Error as e:
         print(f"The error '{e}' occurred")
 
@@ -59,58 +63,52 @@ def create_formatted_insert_query(table_name, values):
     '''
     return insert_query
 
-if __name__ == "__main__":
-    connection = create_connection('localhost', 'root', 'password')
-
-    # Create a new database
-    # create_database_query = "CREATE DATABASE yfinance"
-    # create_database(connection, create_database_query)
-
-    # Create a new table
-
-    # create_table_query = """
-    # USE yfinance;
-    # CREATE TABLE test2 (
-    # employee_id INT,
-    # employee_name VARCHAR(50)
-    # );
-    # """
-    # print(create_table_query)
-    # execute_query(connection, create_table_query)
-
-    # drop_table_query = """
-    # USE yfinance;
-    # DROP TABLE test2;
-    # """
-    # execute_query(connection, drop_table_query)
-
-    # insert_string = """
-    # USE yfinance;
-    # INSERT INTO test VALUES (
-    #     "2023-07-07",
-    #     168.23,
-    #     168.64,
-    #     166.00,
-    #     167.14,
-    #     1315400,
-    #     0.00,
-    #     0.00
-    # );
-    # """
-    # execute_query(connection, insert_string)
-
-
-    with open('csv_data\ZTS_1y.csv', 'r') as infile:
+def write_csv_to_db(connection, file_path):
+    """
+    Reads .csv file from file_path and creates a set of queries to insert table data
+        
+        Parameters:
+            connection PooledMySQLConnection: Database connection object
+            file_path (str): path to a .csv file
+    """
+    symbol = os.path.split(file_path)[-1].split("_")[0]
+    with open(file_path, 'r') as infile:
         csvreader = csv.reader(infile)
         for index, row in enumerate(csvreader):
             if index == 0:
-                formatted_table_query = create_formatted_table_query('test', row)
+                connection.reconnect()
+                formatted_table_query = create_formatted_table_query(symbol, row)
                 # print(formatted_table_query)
                 execute_query(connection, formatted_table_query)
             else:
                 connection.reconnect()
-                insert_query = create_formatted_insert_query('test', row)
+                insert_query = create_formatted_insert_query(symbol, row)
                 # print(insert_query)
                 execute_query(connection, insert_query)
 
+def get_csv_file_dir(dir_path) -> list:
+    """
+    Reads the directory at dir_path and returns a list of paths to .csv files
+        
+        Parameters:
+            dir_path (str): path to a directory containing .csv files
+        Returns:
+            A list of .csv file paths
+    """
+    csv_list = []
+    if os.path.isdir(dir_path):
+        csv_list = os.listdir(dir_path)
+        csv_list = [os.path.join(dir_path,csv) for csv in csv_list if csv.endswith('.csv')]
+    return csv_list
+
+if __name__ == "__main__":
+
+    connection = create_connection('localhost', 'root', 'password')
+    csv_list = get_csv_file_dir('.\csv_data')
+
+    #TODO : BUG Fatal Python error: none_dealloc: deallocating None: bug likely caused by a refcount error in a C extension
+    for index, csv_path in enumerate(csv_list):
+            write_csv_to_db(connection, csv_path)
+    
     connection.close()
+    
